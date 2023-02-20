2f41dc3179dd52ed7dba40b4e859d47cd2adf026
Yarik <36842297+yaroGOR@users.noreply.github.com>
2023-02-20 20:21:07 +0200

    try ro connect pg

--- database/connectDB.js
+++ database/connectDB.js
@@ -1,15 +1,26 @@
 const pg =require('pg')
-
 const Pool = pg.Pool
-const pool = new Pool({
+const connURL = 'postgres://poster:lFiogOtCjCoWDTIfyad1WIJGef2afj5b@dpg-cfpqt082i3mo4bsqe4o0-a/poster_99c1'
+function connect() {
+  console.log('connect = ', process.env.PORT)
+  if (!process.env.PORT) {
+    console.log('running localhost')
+    return new Pool({
   
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DB,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
-
- 
-})
+      user: process.env.PG_USER,
+      host: process.env.PG_HOST,
+      database: process.env.PG_DB,
+      password: process.env.PG_PASSWORD,
+      port: process.env.PG_PORT,
+    
+     
+    })
+  } else {
+    console.log('connecting db on server')
+    return new pg.Client(connURL)
+  }
+  
+}
+let pool =  connect()
 
 module.exports =  pool;
